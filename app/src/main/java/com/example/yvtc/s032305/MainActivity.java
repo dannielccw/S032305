40e260c4c189d7bf61df3c9b3ec26f5f9b0f58cf
dannielccw <dannielccw@gmail.com>
2017-03-23 16:20:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/s032305/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.yvtc.s032305;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("選項一");
+        menu.add("選項二");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
