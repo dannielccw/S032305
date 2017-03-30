70e01a935efa4c81dcfc5176e00871af1e085444
dannielccw <dannielccw@gmail.com>
2017-03-30 09:47:33 +0800

    長按TextView會秀出選項可選擇　少用

--- app/src/main/java/com/example/yvtc/s032305/MainActivity.java
+++ app/src/main/java/com/example/yvtc/s032305/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.yvtc.s032305;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
+
+        registerForContextMenu(tv);
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if (v.getId() == R.id.textView)
+        {
+            menu.add("AA");
+            menu.add("BB");
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
